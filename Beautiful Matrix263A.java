daa992cc96423d27b89095bd297b0147b4313f9d
sourabhsahu0325 <sahusourabh0325@gmail.com>
2026-08-06 23:13:02 +0530

    Create Beautiful Matrix263A.java

---
+++ Beautiful Matrix263A.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row = 0, col = 0;
+
+        // Read the 5x5 matrix
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                int x = sc.nextInt();
+                if (x == 1) {
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+
+        // Calculate minimum moves
+        int moves = Math.abs(row - 3) + Math.abs(col - 3);
+
+        System.out.println(moves);
+
+        sc.close();
+    }
+}
