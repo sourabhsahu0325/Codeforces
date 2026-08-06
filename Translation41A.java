97a523fb7d8af90bc6841cebfe038e28a06967c5
sourabhsahu0325 <sahusourabh0325@gmail.com>
2026-08-06 23:03:48 +0530

    Create Translation41A.java

---
+++ Translation41A.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+
+        String reversed = new StringBuilder(s).reverse().toString();
+
+        if (reversed.equals(t)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
