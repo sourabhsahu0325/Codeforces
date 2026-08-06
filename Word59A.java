5c4b171937dab5aacd02f84dee8f306c06208cc0
sourabhsahu0325 <sahusourabh0325@gmail.com>
2026-08-06 23:19:17 +0530

    Create Word59A.java

---
+++ Word59A.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        int upper = 0;
+        int lower = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isUpperCase(ch))
+                upper++;
+            else
+                lower++;
+        }
+
+        if (upper > lower)
+            System.out.println(s.toUpperCase());
+        else
+            System.out.println(s.toLowerCase());
+    }
+}
