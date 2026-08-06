d0fd6590e351ca775054a65241a20cca9bd8558d
sourabhsahu0325 <sahusourabh0325@gmail.com>
2026-08-06 23:02:26 +0530

    Create WayTooLongWords71A.java

---
+++ WayTooLongWords71A.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        while (n-- > 0) {
+            String word = sc.next();
+
+            if (word.length() > 10) {
+                System.out.println(word.charAt(0) +
+                        String.valueOf(word.length() - 2) +
+                        word.charAt(word.length() - 1));
+            } else {
+                System.out.println(word);
+            }
+        }
+
+        sc.close();
+    }
+}
